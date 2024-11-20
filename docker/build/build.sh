1d0c678dab49ca8e46d619dd29f6fd9ce6b56d5a
hi34568254@aliyun.com <hi34568254@aliyun.com>
2024-11-20 11:42:33 +0800

    修改打包脚本

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -8,6 +8,16 @@ git clone https://github.com/feishu/xgen.git /app/xgen-v1.0 && \
 git clone https://github.com/feishu/yao-init.git /app/yao-init && \
 git clone https://github.com/feishu/yao.git /app/yao
 
+# 解压
+cd /app/v8go/deps && \
+unzip ./darwin_arm64/libv8.a.zip -d ./darwin_arm64 && \
+unzip ./darwin_x86_64/libv8.a.zip -d ./darwin_x86_64 && \
+unzip ./linux_arm64/libv8.a.zip -d ./linux_arm64 && \
+unzip ./linux_x86_64/libv8.a.zip -d ./linux_x86_64 && \
+ls -l ./darwin_arm64 && \
+ls -l ./darwin_x86_64 && \
+ls -l ./linux_arm64 && \
+ls -l ./linux_x86_64
 
 cd /app/yao && \
 export VERSION=$(cat share/const.go  |grep 'const VERSION' | awk '{print $4}' | sed "s/\"//g") 
