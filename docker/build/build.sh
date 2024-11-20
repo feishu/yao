9d96e8373e60b2707be817df32f3d07709ec93c0
hi34568254@aliyun.com <hi34568254@aliyun.com>
2024-11-20 11:42:33 +0800

    修改打包脚本

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -8,6 +8,16 @@ git clone https://github.com/yaoapp/cui.git /app/cui-v1.0 && \
 git clone https://github.com/yaoapp/yao-init.git /app/yao-init && \
 git clone https://github.com/feishu/yao.git /app/yao
 
+# 解压
+cd /app/v8go/deps && \
+unzip ./darwin_arm64/libv8.a.zip -d ./darwin_arm64 && \
+unzip ./darwin_x86_64/libv8.a.zip -d ./darwin_x86_64 && \
+unzip ./linux_arm64/libv8.a.zip -d ./linux_arm64 && \
+unzip ./linux_x86_64/libv8.a.zip -d ./linux_x86_64 && \
+ls -l ./darwin_arm64 && \
+ls -l ./darwin_x86_64 && \
+ls -l ./linux_arm64 && \
+ls -l ./linux_x86_64
 
 files=$(find /app/v8go -name "libv8*.zip")
 for file in $files; do
