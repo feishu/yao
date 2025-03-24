f393289387f041b7dd95372a6d22577dc5e1209e
lanyunfeng <fengming1@163.com>
2025-03-24 14:05:59 +0800

    feat(im): 更新IM服务配置，添加新API接口
    - 将基础服务信息类型更改为通用类型，增强代码的可维护性。
    - 更新API版本为2020-12-01，并添加多个新API接口以支持IM服务的扩展功能。
    - 重新组织API接口定义，确保与最新的服务需求一致。

--- volcengine/service/im/config.go
+++ volcengine/service/im/config.go
@@ -5,17 +5,16 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/yaoapp/yao/volcengine/base"
+	common "github.com/yaoapp/yao/volcengine/base"
 )
 
 const (
 	ServiceName    = "rtc"
 	DefaultTimeout = 10 * time.Second
-	ApiVersion     = "2020-12-01"
 )
 
 var (
-	ServiceInfoMap = map[string]base.ServiceInfo{
+	ServiceInfoMap = map[string]common.ServiceInfo{
 		"cn-north-1": {
 			Timeout: DefaultTimeout,
 			Scheme:  "https",
@@ -23,7 +22,7 @@ var (
 			Header: http.Header{
 				"Accept": []string{"application/json"},
 			},
-			Credentials: base.Credentials{
+			Credentials: common.Credentials{
 				Region:  "cn-north-1",
 				Service: ServiceName,
 			},
@@ -35,20 +34,20 @@ var (
 			Header: http.Header{
 				"Accept": []string{"application/json"},
 			},
-			Credentials: base.Credentials{
+			Credentials: common.Credentials{
 				Region:  "ap-southeast-1",
 				Service: ServiceName,
 			},
 		},
 	}
+	ApiListInfo = map[string]*common.ApiInfo{
 
-	ApiListInfo = map[string]*base.ApiInfo{
 		"GetConversationMarks": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"GetConversationMarks"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 		"MarkConversation": {
@@ -56,7 +55,7 @@ var (
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"MarkConversation"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 		"ModifyParticipantReadIndex": {
@@ -64,23 +63,15 @@ var (
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"ModifyParticipantReadIndex"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
 			},
 		},
-		"BatchAddBlockParticipants": {
+		"ScanConversationParticipantList": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
-				"Action":  []string{"BatchAddBlockParticipants"},
-				"Version": []string{ApiVersion},
-			},
-		},
-		"BatchDeleteBlockParticipants": {
-			Method: http.MethodPost,
-			Path:   "/",
-			Query: url.Values{
-				"Action":  []string{"BatchDeleteBlockParticipants"},
-				"Version": []string{ApiVersion},
+				"Action":  []string{"ScanConversationParticipantList"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 		"BatchGetBlockParticipants": {
@@ -88,7 +79,151 @@ var (
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"BatchGetBlockParticipants"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"IsUserInConversation": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"IsUserInConversation"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchModifyConversationParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchModifyConversationParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchDeleteConversationParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchDeleteConversationParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchDeleteBlockParticipants": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchDeleteBlockParticipants"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchGetConversationParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchGetConversationParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchGetWhitelistParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchGetWhitelistParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchAddWhitelistParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchAddWhitelistParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchAddManager": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchAddManager"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchAddConversationParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchAddConversationParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchAddBlockParticipants": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchAddBlockParticipants"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchRemoveWhitelistParticipant": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchRemoveWhitelistParticipant"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchRemoveManager": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchRemoveManager"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchUpdateLiveParticipants": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchUpdateLiveParticipants"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetParticipantReadIndex": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetParticipantReadIndex"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetConversationUserCount": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetConversationUserCount"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"QueryLiveParticipantStatus": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"QueryLiveParticipantStatus"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"ModifyConversation": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"ModifyConversation"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"ModifyConversationSetting": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"ModifyConversationSetting"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 		"CreateConversation": {
@@ -96,39 +231,63 @@ var (
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"CreateConversation"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
 			},
 		},
-		"DeleteConversation": {
+		"BatchGetConversations": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
-				"Action":  []string{"DeleteConversation"},
-				"Version": []string{ApiVersion},
+				"Action":  []string{"BatchGetConversations"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
-		"GetConversation": {
+		"GetConversationSetting": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
-				"Action":  []string{"GetConversation"},
-				"Version": []string{ApiVersion},
+				"Action":  []string{"GetConversationSetting"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
-		"SendMessage": {
+		"GetUserConversations": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
-				"Action":  []string{"SendMessage"},
-				"Version": []string{ApiVersion},
+				"Action":  []string{"GetUserConversations"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
-		"GetMessages": {
+		"DestroyConversation": {
 			Method: http.MethodPost,
 			Path:   "/",
 			Query: url.Values{
-				"Action":  []string{"GetMessages"},
-				"Version": []string{ApiVersion},
+				"Action":  []string{"DestroyConversation"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"ModifyMessage": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"ModifyMessage"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetConversationMessages": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetConversationMessages"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"DeleteConversationMessage": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"DeleteConversationMessage"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 		"DeleteMessage": {
@@ -136,7 +295,191 @@ var (
 			Path:   "/",
 			Query: url.Values{
 				"Action":  []string{"DeleteMessage"},
-				"Version": []string{ApiVersion},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"SendMessage": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"SendMessage"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetMessages": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetMessages"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"RecallMessage": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"RecallMessage"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchSendMessage": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchSendMessage"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetMessagesReadReceipt": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetMessagesReadReceipt"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"DeleteFriend": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"DeleteFriend"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"UpdateFriend": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"UpdateFriend"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"UpdateBlackList": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"UpdateBlackList"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"ListFriend": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"ListFriend"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"QueryOnlineStatus": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"QueryOnlineStatus"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetBlackList": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetBlackList"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"IsFriend": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"IsFriend"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"IsInBlackList": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"IsInBlackList"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"AddFriend": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"AddFriend"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"AddBlackList": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"AddBlackList"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"GetAppToken": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"GetAppToken"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"RemoveBlackList": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"RemoveBlackList"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"UserBroadcast": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"UserBroadcast"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchUpdateUserTags": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchUpdateUserTags"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"RegisterUsers": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"RegisterUsers"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"UnRegisterUsers": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"UnRegisterUsers"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchUpdateUser": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchUpdateUser"},
+				"Version": []string{"2020-12-01"},
+			},
+		},
+		"BatchGetUser": {
+			Method: http.MethodPost,
+			Path:   "/",
+			Query: url.Values{
+				"Action":  []string{"BatchGetUser"},
+				"Version": []string{"2020-12-01"},
 			},
 		},
 	}
