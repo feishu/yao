8d3be0ed1c977551449458ead720edba378ac7cd
lanyunfeng <fengming1@163.com>
2025-10-22 11:06:01 +0800

    chore(middleware): 注释掉不再使用的Xgen和__yao_admin_root路径处理逻辑

--- service/middleware.go
+++ service/middleware.go
@@ -32,20 +32,20 @@ func withStaticFileServer(c *gin.Context) {
 	}
 
 	// Xgen 1.0
-	if length >= AdminRootLen && c.Request.URL.Path[0:AdminRootLen] == AdminRoot {
-		c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, c.Request.URL.Path[0:AdminRootLen-1])
-		XGenFileServerV1.ServeHTTP(c.Writer, c.Request)
-		c.Abort()
-		return
-	}
+	// if length >= AdminRootLen && c.Request.URL.Path[0:AdminRootLen] == AdminRoot {
+	// 	c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, c.Request.URL.Path[0:AdminRootLen-1])
+	// 	XGenFileServerV1.ServeHTTP(c.Writer, c.Request)
+	// 	c.Abort()
+	// 	return
+	// }
 
 	// __yao_admin_root
-	if length >= 18 && c.Request.URL.Path[0:18] == "/__yao_admin_root/" {
-		c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, "/__yao_admin_root")
-		XGenFileServerV1.ServeHTTP(c.Writer, c.Request)
-		c.Abort()
-		return
-	}
+	// if length >= 18 && c.Request.URL.Path[0:18] == "/__yao_admin_root/" {
+	// 	c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, "/__yao_admin_root")
+	// 	XGenFileServerV1.ServeHTTP(c.Writer, c.Request)
+	// 	c.Abort()
+	// 	return
+	// }
 
 	// Rewrite
 	for _, rewrite := range rewriteRules {
